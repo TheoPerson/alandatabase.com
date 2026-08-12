0020fba4107c403068fdd68a4cb20226a8ca6496
Theo Person <theo@thealansdatabase.com>
2026-08-12 04:48:08 +0200

    fix: set ssr.noExternal: true in vite.config.ts to bundle @sveltejs/kit into Netlify lambda function

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	ssr: {
+		noExternal: true,
 		external: ['@electric-sql/pglite']
 	},
 	test: {
